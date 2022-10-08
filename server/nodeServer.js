5daecf27d7650d2d63cc7984e21cc7def1d8fd21
nischalBks <nischal.gupta@bksgold.com>
2022-10-08 19:19:47 +0530

    Deployed server + Sample Crud from Client

--- server/nodeServer.js
+++ server/nodeServer.js
@@ -2,16 +2,18 @@ const express = require("express");
 const app = express();
 require('./DB-connection')
 const cors = require('cors')
-const authentication = require('./models/Auth');
-const port = 3690
 const authRouter = require("./routes/Auth")
+require('dotenv').config()
 //=============================================
 app.use(cors())
 app.use(express.json())
 app.use("/auth",authRouter)
 
+app.get('/',(req,res)=>{
+    res.send('------------ WELCOME_ADEPT_BANIYA -------------')
+})
 //=============================================
 
 //=============================================
-app.listen(port, () => { console.log(`chal gaya node server ${port} ispe`) })
+app.listen(process.env.PORT, () => { console.log(`chal gaya node server ${process.env.PORT} ispe`) })
 module.exports = app;
